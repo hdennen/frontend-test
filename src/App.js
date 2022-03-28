16ca8bdee49c016233d009e781d5d5ac41fb713e
Harry Dennen <hdennen@gmail.com>
2022-03-27 21:35:42 -0400

    add filter elements

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
 import './App.css'
 import HousingGradeTable from './components/HousingGradeTable.js'
+import SelectOwnership from './components/Filter.js'
+import SelectQuarter from './components/Filter.js'
+import SelectTerm from './components/Filter.js'
+import SelectYear from './components/Filter.js'
 import { getData } from './request/api.js'
 
-function separateByGrade(data) {
-  const dataByGrade = [];
-  
-  data.forEach(element => {
-    if (!element.grade) return;  
+function separateData(data) {
+  const dataByGrade = [],  
+        ownershipValues = new Set(),
+        quarterValues = new Set(),
+        termValues = new Set(),
+        yearValues = new Set();
+
+  data.forEach(element => { // who's for premature optimization? let's do it in the first pass.
+    if (!element.grade) return;
+
+    ownershipValues.add(element.homeOwnership);
+    quarterValues.add(element.quarter);
+    termValues.add(element.term);
+    yearValues.add(element.year);
 
     // terrible ideas
     if (!dataByGrade[parseInt(element.grade)-1]) dataByGrade[parseInt(element.grade)-1] = [];
     dataByGrade[parseInt(element.grade)-1].push(element);
   });
 
-  return dataByGrade;
+  return {
+    dataByGrade,
+    ownershipValues: [...ownershipValues],
+    quarterValues: [...quarterValues],
+    termValues: [...termValues],
+    yearValues: [...yearValues]
+  };
 }
 
 function aggregateBalances(dataByGrade) { // make it totally unreadable
@@ -27,20 +46,34 @@ class App extends Component {
     // this state will go in a store so we can reset/recall api
     this.state = {
       dataByGrade: [], 
-      aggregateBalanceByGrade: []
+      aggregateBalanceByGrade: [],
+      ownershipValues: [],
+      quarterValues: [],
+      termValues: [],
+      yearValues: []
     };
   }
 
   componentDidMount() {
     getData().then(result => {
-      const dataByGrade = separateByGrade(result),
-      aggregateBalanceByGrade = aggregateBalances(dataByGrade)
+      const { dataByGrade,
+              ownershipValues,
+              quarterValues,
+              termValues,
+              yearValues } = separateData(result),
+              aggregateBalanceByGrade = aggregateBalances(dataByGrade);
+
+      console.log(dataByGrade);
 
       this.setState({
         dataByGrade,
+        ownershipValues,
+        quarterValues,
+        termValues,
+        yearValues,
         aggregateBalanceByGrade
       })
-    }) 
+    })
   }
 
   render() {
@@ -49,14 +82,14 @@ class App extends Component {
       <div className="App">
         <p>CHARTS AND GRAPHS</p>
         <HousingGradeTable aggregates = {this.state.aggregateBalanceByGrade}/>
+        <SelectOwnership values = {this.state.ownershipValues} title = 'Ownership'/>
+        <SelectQuarter values = {this.state.quarterValues} title = 'Quarter'/>
+        <SelectTerm values = {this.state.termValues} title = 'Term'/>
+        <SelectYear values = {this.state.yearValues} title = 'Year'/>
       </div>
       </>
     )
   }
 }
-
-// fetch
-// wrangle
-// render
 
 export default App
